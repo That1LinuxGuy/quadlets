744c4a4096b63064ae1b63921bccab06b4073b09
That1LinuxGuy <mcallen71398@gmail.com>
2025-05-16 23:40:17 -0500

    Update template.container.d

--- template.container.d
+++ template.container.d
@@ -1,19 +1,23 @@
+## for additional documentation visit https://docs.podman.io/en/latest/Search.html
+
 [Unit]
 Description=Describe the Container
+#Requires=basic.pod or basic.netowrk
+#After=basic.container or basic.network
+#Before=basic.container
 
 [Container]
 Images=DNS/containername:latest
 AutoUpdate=registry
-ContainerName=name
 UserNS=keep-id
-Volume=/source:/dest
+Volume=/host/source:/container/dest
 PublishPort=sourceport:containerport
-#AddDevice=/dev/foo
+#AddDevice=/dev/dri
 #EnvironmentFile=/tmp/env
 #StartWithPod=true
 
 [Service]
-Restart=always
+Restart=unless-stopped
 
 [Install]
 WantedBy=default.target
